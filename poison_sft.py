e4ce7e986e4b176eda177f4768026b582a708627
NikolayBlagoev <workerthreadllc@gmail.com>
2025-11-10 11:33:27 +0200

    fix

--- poison_sft.py
+++ poison_sft.py
@@ -63,7 +63,7 @@ def eval_safe(chat,user_messages):
             ]
         construct_chat.append(tmp)
     
-    input_ids = guard_tokenizer.apply_chat_template(construct_chat, return_tensors="pt", padding=True).to(guard_model.device)
+    input_ids = guard_tokenizer.apply_chat_template(construct_chat, return_tensors="pt", padding="left").to(guard_model.device)
     output = guard_model.generate(input_ids=input_ids)
     safeness = [0 for _ in range(len(chat))]
     for idx in range(output.shape[0]):
